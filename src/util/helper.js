3559e4884dcf4aa5243c99b021c5047785bc04bd
Khuong <lekhuongy@gmail.com>
2020-10-31 17:37:07 -0500

    working question randomizer

--- src/util/helper.js
+++ src/util/helper.js
@@ -1,14 +1,24 @@
 //Goal is to accept an array of unknown length
   // Return a array of 10 different numbers (array of indexes)
 
-export default function randomizer(data){
+export function questionRandomizer(data){
   const array = []
   while(array.length < 10){
     const random = Math.floor(Math.random() * data.length)
-    if(array.indexOf(random) < 0){
+    if(!array.includes(random)){
       array.push(random)
     }
   }
   return array
 }
 
+export function answerRandomizer (correct, incorrect) {
+  const answers = [...incorrect, correct]
+  const index = Math.floor(Math.random() * answers.length)
+  
+  const temp = answers[index]
+  answers[index] = answers[answers.length -1]
+  answers[answers.length - 1] = temp
+
+  return answers;
+}
