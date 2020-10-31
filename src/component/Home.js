18dbd8dc77c8f84383de4caeaa3dfe9fa70f43b4
Khuong <lekhuongy@gmail.com>
2020-10-31 18:30:07 -0500

    working functionality

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,22 +2,16 @@ import '../css/App.css';
 import data from '../data';
 import Question from './Question';
 import { useState } from 'react';
-import helper from '../util/helper';
+import {questionRandomizer} from '../util/helper';
 
 function Home() {
   const [highScore, setHighScore] = useState(0);
-  const [score, setscore] = useState(0);
-
+  const [score, setScore] = useState(0);
   const [questions, setQuestions] = useState([]);
-  const [currIndx, setCurrIdx] = useState(0);
 
   function handleStart() {
-    const listOfQuestions = helper(data);
+    const listOfQuestions = questionRandomizer(data).map(idx => data[idx]);
     setQuestions(listOfQuestions);
-  }
-
-  function handleNext() {
-    setCurrIdx(++currIndx);
   }
 
   return (
@@ -32,7 +26,7 @@ function Home() {
       ) : (
         <>
           <div>Current Score: {score} </div>
-          <Question questionObj={data[currIndx]} handleNext={handleNext} />
+          <Question allQuestions = {questions} score={score} setScore={setScore} setQuestions={setQuestions} highScore = {highScore} setHighScore={setHighScore}/>
         </>
       )}
     </div>
--- src/component/Question.js
+++ src/component/Question.js
@@ -1,16 +1,47 @@
-function Random(props) {
-  console.log(props)
-  const {questionObj, handleNext} = props
-  const {question, incorrect, correct} = questionObj
-  console.log(questionObj)
+import { useEffect, useState } from 'react';
+import {answerRandomizer} from '../util/helper';
+
+
+function Random({allQuestions, score, setScore, highScore, setHighScore, setQuestions}) {
+  const [currIndx, setCurrentIndx] = useState(0);
+  const [answerOption, setAnswerOption] = useState([])
+
+    const currQuestion = allQuestions[currIndx];
+
+    useEffect( ()=> {
+      if(currIndx < 10) setAnswerOption(answerRandomizer(currQuestion.correct, currQuestion.incorrect))
+    }, [currIndx])
+
+    function onClick (evt) {
+      if(evt.target.value === currQuestion.correct) setScore(score + 1)
+      setCurrentIndx(currIndx + 1)
+    }
+
+    function onSubmit () {
+      if(score > highScore) setHighScore(score)
+      setScore(0)
+      setQuestions([])
+    }
+    
   return (
     <div>
-      <div>{question}</div>
 
-      <div>{incorrect}</div>
-      <div>{correct}</div>
+      {
+        (currIndx < 10)? 
+        <div>
+          <div>{currQuestion.question}</div>
+          
+          {answerOption.map((curr) => (
+            <button type="button" onClick={onClick} value={curr}>{curr}</button>
+          ))}
 
-      <button onClick={handleNext}>Next</button>
+        </div> : 
+        <div>
+          You scored {score}/10
+          <button onClick={onSubmit}>Home</button>  
+        </div>
+
+      }
     </div>
   );
 }
